3e80369caf6ae3559b6a38479c836180ff59d39d
gabrii3lmao <gabrielmluz56@gmail.com>
2026-04-18 17:27:26 -0300

    setting up some things before deploy

--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,28 @@ import Router from "./routes/Router.js";
 import mongoose from "mongoose";
 import "dotenv/config";
 import cors from "cors";
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 const app = express();
 
-
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL! || "", "http://localhost:5173"],
     credentials: true,
   }),
 );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
-app.use(Router);
+app.use("/api", Router);
 
-async function startServer() {
+async function bootstrap() {
   try {
     await mongoose.connect(process.env.MONGO_URL!);
     console.log("Connected to MongoDB!");
-
+    const PORT = process.env.PORT || 3000;
     app.listen(3000, () => {
-      console.log("Server is running on http://localhost:3000");
+      console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
@@ -39,4 +32,4 @@ async function startServer() {
   }
 }
 
-startServer();
+bootstrap();
